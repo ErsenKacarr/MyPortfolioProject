4684b2c91bb64605012c252556298201dbd55fd9
ErsenKacarr <ersenamzn@gmail.com>
2024-11-21 03:46:20 +0300

    Add project files.

---
+++ MyPortfolioProject/Controllers/PortfolioController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioProject.DAL.Context;
+using MyPortfolioProject.DAL.Entities;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult PortfolioList()
+        {
+            var values = context.Portfolios.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreatePortfolio()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePortfolio(Portfolio Portfolio)
+        {
+            context.Portfolios.Add(Portfolio);
+            context.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+
+        public IActionResult DeletePortfolio(int id)
+        {
+            var value = context.Portfolios.Find(id);
+            context.Portfolios.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdatePortfolio(int id)
+        {
+            var value = context.Portfolios.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePortfolio(Portfolio Portfolio)
+        {
+            context.Portfolios.Update(Portfolio);
+            context.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+    }
+}
